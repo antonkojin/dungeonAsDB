a652bde3e00b36a0f7f62d310f4118129e24a7ad
Anton Kozhin <antonkojin@gmail.com>
2017-07-19 19:39:56 +0200

    api specification

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,15 @@
 DROP TABLE IF EXISTS character_items;
+DROP TABLE IF EXISTS room_enemies;
+DROP TABLE IF EXISTS room_items;
+ALTER TABLE rooms DROP CONSTRAINT IF EXISTS rooms_dungeon_fkey;
+DROP TABLE IF EXISTS dungeons;
+DROP TABLE IF EXISTS gates;
+DROP TABLE IF EXISTS rooms;
 DROP TABLE IF EXISTS characters;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS items;
 DROP TYPE IF EXISTS ITEM_CATEGORY;
 DROP TABLE IF EXISTS enemies;
-DROP TABLE IF EXISTS rooms;
-DROP TABLE IF EXISTS gates;
 
 CREATE TYPE ITEM_CATEGORY AS ENUM ('attack', 'defence', 'consumable');
 CREATE TABLE items (
@@ -17,7 +21,6 @@ CREATE TABLE items (
         wisdom SMALLINT NOT NULL, -- [-6,6]
         hit_points SMALLINT NOT NULL, -- [-6,6]
 	category ITEM_CATEGORY NOT NULL
-
 );
 
 CREATE TABLE users (
@@ -44,12 +47,27 @@ CREATE TABLE characters (
 --	hit_points = constitution + bonus
 );
 
+CREATE TABLE rooms (
+	-- two special rooms: start_room, final_room
+	id SERIAL PRIMARY KEY,
+	description VARCHAR NOT NULL,
+	dungeon INTEGER NOT NULL
+);
+
+CREATE TABLE dungeons (
+	id SERIAL PRIMARY KEY,
+	character INTEGER REFERENCES characters(id) NOT NULL,
+	current_room INTEGER REFERENCES rooms(id) NOT NULL,
+	start_room INTEGER REFERENCES rooms(id) NOT NULL,
+	final_room INTEGER REFERENCES rooms(id) NOT NULL
+);
+
+ALTER TABLE rooms ADD CONSTRAINT rooms_dungeon_fkey FOREIGN KEY (dungeon) REFERENCES dungeons(id);
+
 CREATE TABLE character_items (
 	id SERIAL PRIMARY KEY,
 	character INTEGER REFERENCES characters(id) NOT NULL,
-	item INTEGER REFERENCES items(id) NOT NULL,
-	times SMALLINT NOT NULL,
-	UNIQUE (character, item)
+	item INTEGER REFERENCES items(id) NOT NULL
 );
 
 CREATE TABLE enemies (
@@ -58,20 +76,30 @@ CREATE TABLE enemies (
 	description VARCHAR NOT NULL,
 	attack SMALLINT NOT NULL,
 	defence SMALLINT NOT NULL,
-	hit_points SMALLINT NOT NULL,
+	initial_hit_points SMALLINT NOT NULL,
 	damage SMALLINT NOT NULL
 );
 
-CREATE TABLE rooms (
+CREATE TABLE room_enemies (
 	id SERIAL PRIMARY KEY,
-	description VARCHAR NOT NULL
+	room INTEGER REFERENCES rooms(id) NOT NULL,
+	enemy INTEGER REFERENCES enemies(id) NOT NULL,
+	current_hit_points SMALLINT NOT NULL
+);
+
+CREATE TABLE room_items (
+	id SERIAL PRIMARY KEY,
+	room INTEGER REFERENCES rooms(id) NOT NULL,
+	item INTEGER REFERENCES items(id) NOT NULL,
+	hidden BOOLEAN NOT NULL
 );
 
 CREATE TABLE gates (
-
+	id SERIAL PRIMARY KEY,
+	room_from INTEGER REFERENCES rooms(id) NOT NULL,
+	room_to INTEGER REFERENCES rooms(id) NOT NULL,
+	hidden BOOLEAN NOT NULL
 );
-
-
 
 
 
--- rest/app.py
+++ rest/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,13 @@ def hello_world():
         'name': 'anton',
         'mail': 'anton@example.com'
     })
+
+@app.route('/users/', methods=['POST'])
+def signup():
+	return jsonify({
+		'email': request.form['email'],
+		'nickname': request.form['nickname']
+	}), 201
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
