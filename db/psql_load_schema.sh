80d562a68c3cf0533ef26fd0afac2aea14cc32c2
Anton Kozhin <antonkojin@gmail.com>
2017-07-21 17:23:56 +0200

    psql load schema into db

--- db/psql_load_schema.sh
+++ db/psql_load_schema.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# docker-compose up -d db;
-docker-compose exec db \
-	psql \
-		--echo-errors \
-		--dbname=dungeon_as_db \
-		--file=/code/schema.sql \
-		--username=dungeon_as_db_superuser ;
-# docker-compose stop db;
+psql \
+    --echo-errors \
+    --dbname=dungeon_as_db \
+    --file=/code/schema.sql \
+    --username=dungeon_as_db_superuser
 
