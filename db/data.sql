2fef57d00cf3c8d3be8cb41b231be3d41bb7a3e8
Anton Kozhin <antonkojin@gmail.com>
2017-07-26 15:18:56 +0200

    generate dungeon

---
+++ db/data.sql
@@ -0,0 +1,34 @@
+INSERT INTO users (email, nickname, password_hash)
+VALUES ('test@test.com', 'nickname', 'password_hash');
+
+INSERT INTO characters (name, description, strength, intellect, dexterity, constitution, "user")
+VALUES ('test_name', 'test_description', 3, 3, 3, 3, 'test@test.com');
+
+/*
+INSERT INTO dungeons(character)
+VALUES(1);
+*/
+
+INSERT INTO rooms_descriptions (description)
+VALUES ('[description test]');
+INSERT INTO rooms_descriptions (id, description)
+VALUES (0, 'FINAL_ROOM');
+
+INSERT INTO items (name, description, attack, defence, wisdom, hit_points, category)
+VALUES 
+    ('cons1', 'description1', 0, 0, 0, 0, 'consumable'),
+    ('cons2', 'description2', 0, 0, 0, 0, 'consumable'),
+    ('cons3', 'description3', 0, 0, 0, 0, 'consumable'),
+    ('cons4', 'description4', 0, 0, 0, 0, 'consumable'),
+    ('sword1', 'description5', 0, 0, 0, 0, 'attack'),
+    ('shield1', 'description6', 0, 0, 0, 0, 'defence');
+
+INSERT INTO enemies (name, description, attack, defence, 
+    initial_hit_points, damage
+) VALUES 
+    ('enemy1', 'description1', 0, 0, 1, 0),
+    ('enemy2', 'description2', 0, 0, 1, 0),
+    ('enemy3', 'description3', 0, 0, 1, 0),
+    ('enemy4', 'description4', 0, 0, 1, 0),
+    ('enemy5', 'description5', 0, 0, 1, 0),
+    ('enemy6', 'description6', 0, 0, 1, 0);
--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,10 @@ DROP TABLE IF EXISTS character_items;
 DROP TABLE IF EXISTS room_enemies;
 DROP TABLE IF EXISTS room_items;
 ALTER TABLE IF EXISTS rooms DROP CONSTRAINT IF EXISTS rooms_dungeon_fkey;
-DROP TABLE IF EXISTS dungeons;
 DROP TABLE IF EXISTS gates;
+DROP TABLE IF EXISTS dungeons;
 DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS rooms_descriptions;
 DROP TABLE IF EXISTS characters;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS items;
@@ -24,12 +25,11 @@ CREATE TABLE items (
 );
 
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
+	email VARCHAR(254) PRIMARY KEY,
 	nickname VARCHAR(20) NOT NULL UNIQUE,
-	email VARCHAR(254) NOT NULL UNIQUE,
 	password_hash CHARACTER(93) NOT NULL
 );
-	
+
 CREATE TABLE characters (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(20) NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE characters (
 	constitution SMALLINT NOT NULL CHECK (strength >= 3 AND strength <= 18),
 	equipped_defence_item INTEGER REFERENCES items(id),
 	equipped_attack_item INTEGER REFERENCES items(id),
-	"user" INTEGER REFERENCES users(id) NOT NULL UNIQUE,
+	"user" VARCHAR(254) REFERENCES users(email) NOT NULL UNIQUE,
 --	attack = (strength + dexterity) / 2 + bonus
 --	defence = (constitution + dextrity) / 2 + bonus
 --	wisdom = intellect + bonus
@@ -51,19 +51,23 @@ CREATE TABLE characters (
 	room_hit_points_bonus SMALLINT
 );
 
+CREATE TABLE rooms_descriptions (
+    id SERIAL PRIMARY KEY,
+	description VARCHAR NOT NULL
+);
+
 CREATE TABLE rooms (
 	-- two special rooms: start_room, final_room
 	id SERIAL PRIMARY KEY,
-	description VARCHAR NOT NULL,
+	description INTEGER NOT NULL REFERENCES rooms_descriptions(id),
 	dungeon INTEGER NOT NULL
 );
 
 CREATE TABLE dungeons (
 	id SERIAL PRIMARY KEY,
-	character INTEGER REFERENCES characters(id) NOT NULL,
-	current_room INTEGER REFERENCES rooms(id) NOT NULL,
-	start_room INTEGER REFERENCES rooms(id) NOT NULL,
-	final_room INTEGER REFERENCES rooms(id) NOT NULL
+	character INTEGER REFERENCES characters(id) NOT NULL UNIQUE,
+	current_room INTEGER REFERENCES rooms(id),
+	final_room INTEGER REFERENCES rooms(id)
 );
 
 ALTER TABLE rooms ADD CONSTRAINT rooms_dungeon_fkey FOREIGN KEY (dungeon) REFERENCES dungeons(id);
@@ -100,9 +104,12 @@ CREATE TABLE room_items (
 
 CREATE TABLE gates (
 	id SERIAL PRIMARY KEY,
+    dungeon INTEGER REFERENCES dungeons(id) NOT NULL,
 	room_from INTEGER REFERENCES rooms(id) NOT NULL,
 	room_to INTEGER REFERENCES rooms(id) NOT NULL,
-	hidden BOOLEAN NOT NULL
+	hidden BOOLEAN NOT NULL,
+    UNIQUE (dungeon, room_from, room_to),
+    UNIQUE (dungeon, room_to, room_from)
 );
 
 DROP FUNCTION IF EXISTS create_character(character varying,character varying,smallint,smallint,smallint,smallint,character varying);
@@ -113,12 +120,9 @@ CREATE FUNCTION create_character(
     intellect SMALLINT,
     dexterity SMALLINT,
     constitution SMALLINT,
-    user_email VARCHAR(254)
+    email VARCHAR(254)
 ) RETURNS void AS $$
-    DECLARE
-        user_id INTEGER;
     BEGIN
-        user_id := (SELECT id FROM "users" WHERE users.email = user_email);
         INSERT INTO characters (
             name, 
             description, 
@@ -134,7 +138,178 @@ CREATE FUNCTION create_character(
             intellect, 
             dexterity, 
             constitution, 
-            user_id
+            email
         );
     END;
 $$ LANGUAGE 'plpgsql';
+
+DROP FUNCTION IF EXISTS create_room(INTEGER);
+CREATE FUNCTION create_room(
+    dungeon INTEGER
+) RETURNS INTEGER AS $$
+    DECLARE
+        max_items CONSTANT SMALLINT := 5;
+        max_enemies CONSTANT SMALLINT := 2;
+        min_enemies CONSTANT SMALLINT := 1;
+        room_id INTEGER;
+    BEGIN
+        INSERT INTO rooms (dungeon, description)
+        VALUES (dungeon, (
+            SELECT id FROM rooms_descriptions 
+            WHERE id != 0 
+            ORDER BY random() 
+            LIMIT 1
+        )) RETURNING id INTO room_id;
+        
+        INSERT INTO room_items (room, hidden, item)
+        SELECT
+            room_id,
+            random() > 0.5, 
+            id
+        FROM items ORDER BY random() 
+        LIMIT floor(random() * (max_items + 1));
+
+        INSERT INTO room_enemies (room, enemy, current_hit_points)
+        SELECT
+            room_id,
+            id,
+            initial_hit_points
+        FROM enemies 
+        ORDER BY random() 
+        LIMIT floor( random() * (max_enemies + 1 - min_enemies) + min_enemies );
+
+        RETURN room_id;
+    END;
+$$ LANGUAGE 'plpgsql';
+
+DROP FUNCTION IF EXISTS generate_rooms(INTEGER);
+CREATE FUNCTION generate_rooms(
+    dungeon_id INTEGER, 
+    OUT start_room INTEGER, 
+    OUT final_room INTEGER
+) AS $$
+    DECLARE
+        n_rooms_visible_path CONSTANT SMALLINT := 5;
+        n_other_rooms CONSTANT SMALLINT := 5;
+        final_room_description CONSTANT INTEGER := 0;
+        previous_room INTEGER;
+    BEGIN
+        -- create start_room
+        SELECT create_room(dungeon_id) INTO start_room;
+        -- create rooms and visible path from start_room to final_room
+        previous_room := start_room;
+        FOR i IN 3..n_rooms_visible_path LOOP
+            SELECT create_room(dungeon_id) INTO final_room;
+            INSERT INTO gates(dungeon, room_from, room_to, hidden)
+            VALUES (dungeon_id, previous_room, final_room, false);
+            previous_room := final_room;
+        END LOOP;
+        -- create some other random room
+        FOR i IN 1..n_other_rooms LOOP
+            PERFORM create_room(dungeon_id);
+        END LOOP;
+        -- create random gates
+        INSERT INTO gates (dungeon, room_from, room_to, hidden)
+        SELECT dungeon_id, legal_gates.from_id, legal_gates.to_id, random() > 0.5
+        FROM (
+            (
+                -- possible gates
+                SELECT "from".id AS from_id, "to".id AS to_id
+                FROM rooms AS "from" JOIN rooms AS "to"
+                ON "from".id < "to".id
+                AND "from".dungeon = dungeon_id
+                AND "to".dungeon = dungeon_id
+            ) EXCEPT (
+                -- existent gates
+                SELECT room_from, room_to FROM gates WHERE dungeon = dungeon_id
+                UNION ALL
+                SELECT room_to, room_from FROM gates WHERE dungeon = dungeon_id
+            )
+        ) AS legal_gates
+        WHERE random() > 0.5; -- with probability of 1/2
+        -- create the final final_room
+        INSERT INTO rooms (dungeon, description)
+        VALUES (dungeon_id, final_room_description)
+        RETURNING id INTO final_room;
+        -- create visible gate from previous final_room to final final_room
+        INSERT INTO gates (dungeon, room_from, room_to, hidden)
+        VALUES (dungeon_id, previous_room, final_room, false);
+    END;
+$$ LANGUAGE 'plpgsql';
+
+DROP FUNCTION IF EXISTS create_dungeon(VARCHAR);
+CREATE FUNCTION create_dungeon(email VARCHAR(254))
+RETURNS void AS $$
+    DECLARE
+        "character" INTEGER;
+        dungeon INTEGER;
+        dungeon_start_room INTEGER;
+        dungeon_final_room INTEGER;
+    BEGIN
+        "character" := (SELECT id FROM characters WHERE "user" = email);
+        INSERT INTO dungeons ("character")
+            VALUES ("character") 
+            RETURNING id INTO dungeon;
+        SELECT start_room, final_room FROM generate_rooms(dungeon)
+            INTO dungeon_start_room, dungeon_final_room;
+        UPDATE dungeons SET
+            final_room = dungeon_final_room,
+            current_room = dungeon_start_room
+            WHERE id = dungeon;
+    END;
+$$ LANGUAGE 'plpgsql';
+
+DROP FUNCTION IF EXISTS dungeon_status(VARCHAR);
+/*
+CREATE FUNCTION dungeon_status(user_email VARCHAR(254))
+RETURNS RECORD AS $$
+    BEGIN
+        return (
+            SELECT
+                -- room
+                rooms_descriptions.description,
+                
+                -- character
+                characters.name,
+                characters.description,
+                characters.strength,
+                characters.intellect,
+                characters.dexterity,
+                caracters.constitution,
+                characters.room_attack_bonus,
+                characters.room_defence_bonus,
+                characters.room_wisdom_bonus,
+                characters.room_hit_points_bonus,
+                characters.equipped_defence_item,
+                characters.equipped_attack_item
+            FROM (
+                dungeons
+                    JOIN
+                rooms
+                    JOIN
+                room_descriptions
+                    JOIN
+                room_items
+                    JOIN
+                items
+                    JOIN
+                room_enemies
+                    JOIN
+                enemies
+                    JOIN
+                gates
+                    JOIN
+                characters
+                    JOIN
+                character_items
+            ) ON dungeons."character" = characters.id
+            AND rooms.id = dungeons.current_room
+            AND rooms_descriptions.id = rooms.description
+            AND room_items.room = dungeons.current_room
+            AND room_items.hidden = false
+            AND room_items.item = items.id
+            WHERE characters.email = user_email
+        );
+    END;
+$$ LANGUAGE 'plpgsql';
+*/
