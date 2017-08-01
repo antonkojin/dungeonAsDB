1cd6c6a34ed167e4d0c07dc8f180154e4cb4d305
Anton Kozhin <antonkojin@gmail.com>
2017-08-01 15:33:07 +0200

    docker's load schema and data

---
+++ db/docker_init_db.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker-compose exec db \
+    psql \
+        --echo-errors \
+        --dbname=dungeon_as_db \
+        --file=/code/schema.sql \
+        --username=dungeon_as_db_superuser && \
+docker-compose exec db \
+    psql \
+        --echo-errors \
+        --dbname=dungeon_as_db \
+        --file=/code/data.sql \
+        --username=dungeon_as_db_superuser
+
--- db/psql_load_schema.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-psql \
-    --echo-errors \
-    --dbname=dungeon_as_db \
-    --file=/code/schema.sql \
-    --username=dungeon_as_db_superuser
-
