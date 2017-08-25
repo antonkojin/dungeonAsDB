519e0fe20f31e61d731d340816967ca6a51a46a1
Anton Kojin <antonkojin@gmail.com>
2017-08-25 19:02:08 +0200

    utility to test the loading of schema, functions and data in DB

--- db/test_functions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-./docker_init_db.sh functions.sql && \
-curl -u test@example.com:test_password localhost:8000/dices -I
---
+++ db/test_load_db.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./docker_init_db.sh schema.sql functions.sql data.sql # && \
+# curl -u test@example.com:test_password localhost:8000/dices -I
