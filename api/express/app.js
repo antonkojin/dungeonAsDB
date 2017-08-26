f3170ef74b27537a7eb58084af12d4cc6b021706
Anton Kojin <antonkojin@gmail.com>
2017-08-27 00:32:38 +0200

    basic template to use

---
+++ api/express/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+// CORS support
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+/*
+// database connection example
+var pgp = require('pg-promise')
+var db = pgp('postgres://username:password@host:port/database')
+
+db.one('SELECT $1 AS value', 123)
+  .then(function (data) {
+    console.log('DATA:', data.value)
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  })
+*/
